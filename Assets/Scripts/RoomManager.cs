f48c98bf62ae9f5532b36400b0382a655c01f818
Admin MediaLab <Admin-MediaLab@uao.edu.co>
2024-02-23 15:06:06 -0500

    Server and Photon Implement

---
+++ Assets/Scripts/RoomManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class RoomManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Connecting...");
+
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+        Debug.Log("Connected to server");
+
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        base.OnJoinedLobby();
+        PhotonNetwork.JoinOrCreateRoom("test", null, null);
+        Debug.Log("Tamo Activo Papi");
+    }
+}
